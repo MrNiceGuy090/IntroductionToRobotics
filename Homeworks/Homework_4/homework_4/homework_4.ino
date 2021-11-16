dae0039934bf8413de9b7cfa78db292226358433
Ştefan Smeu <smeustefan090@gmail.com>
2021-11-16 14:11:42 +0200

    working homework 4

--- Homeworks/Homework_4/homework_4/homework_4.ino
+++ Homeworks/Homework_4/homework_4/homework_4.ino
@@ -1,48 +1,31 @@
+#include <EEPROM.h>
 // joyStick pins
-const int pinSW = 9;
+const int pinSW = 3;
 const int pinX = A0;
 const int pinY = A1;
 
-// 4 digit 7 display pins
-const int dataPin = 12; //DS
-const int latchPin = 11; //STCP
-const int clockPin = 10; //SHCP
-
 // joystick variables
-bool swState =LOW;
-bool lastSwState =LOW;
 int xValue =0;
 int yValue =0;
 bool joyMoved = false;
-int minThreshold=400;
-int maxThreshold=600;
+const int minThreshold=400;
+const int maxThreshold=600;
 
-unsigned int lastDebounceTime =0;
-unsigned int debounceDelay =50;
-
-// 4 digit 7 display variables
-bool isLockedInOnDigit = false;
-int dpPosition = 3;
-bool dpState = false;
-int dpBlinkingInterval = 300;
-long dpBlinkingTime = 0;
-int ledNumber[4] = {1,2,3,4};
-int numberLength = 4;
-
-int digit;
-
+// 4 digit 7 segment display pins
+const int dataPin = 12; //DS
+const int latchPin = 11; //STCP
+const int clockPin = 10; //SHCP
 const int segD1 = 7;
 const int segD2 = 6;
 const int segD3 = 5;
 const int segD4 = 4;
 
+// 4 digit 7 segment display constants
 const int displayCount = 4;
-
 const int displayDigits[] = {
   segD1, segD2, segD3, segD4 
 };
-
-int digitArray[16] = {
+const int digitArray[16] = {
 //A B C D E F G DP 
   B11111100, // 0
   B01100000, // 1
@@ -62,6 +45,14 @@ int digitArray[16] = {
   B10001110, // F
 };
 
+// logic variables
+volatile bool isLockedInOnDigit = false;
+int currentPos = 3;
+int dpBlinkingInterval = 200;
+long dpBlinkingTime = 0;
+int ledNumber[4] = {1,2,3,4};
+int numberLength = 4;
+
 void setup(){
   pinMode(dataPin, OUTPUT);
   pinMode(latchPin, OUTPUT);
@@ -71,84 +62,108 @@ void setup(){
     digitalWrite(displayDigits[i], LOW);
   }
   pinMode(pinSW,INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(pinSW), buttonPressed, FALLING);
+  if ( EEPROM.read(0) != 0xff )
+    for (int i = 0; i < numberLength; ++i )
+        ledNumber[i] = EEPROM.read(i);
   Serial.begin(9600);
 }
 
 void loop(){
+  writeNumber(ledNumber, numberLength, currentPos, isLockedInOnDigit);
 
-  writeNumber(ledNumber, numberLength, dpPosition, isLockedInOnDigit);
-  
-  xValue = analogRead(pinX);
-  if( xValue < minThreshold && joyMoved == false){
-    if(digit >0) {
-      digit--;
-    }
-    else{
-      digit =9;
-    }
-    joyMoved = true;
+  if(isLockedInOnDigit == true){
+    checkJoystickXAxisMovement(pinX, minThreshold, maxThreshold, joyMoved, ledNumber, currentPos );
   }
-  if(xValue > maxThreshold && joyMoved == false){
-    if(digit <9) {
-      digit++;
-    }
-    else{
-      digit =0;
-    }
-    joyMoved = true;
+  else{
+    checkJoystickYAxisMovement(pinY,minThreshold, maxThreshold, joyMoved, currentPos); 
   }
-  if(xValue >= minThreshold && xValue <= maxThreshold){
-    joyMoved = false;
-  }
+}
 
-  bool reading = digitalRead(pinSW);
-  if(reading != lastSwState) {
-    lastDebounceTime = millis();
-  }
-  if( (millis() - lastDebounceTime) > debounceDelay){
-    if(reading != swState) {
-      swState = reading;
-      if(swState == HIGH) {
-        if (isLockedInOnDigit == false){
-          isLockedInOnDigit = true;
-        }
-        else{
-          isLockedInOnDigit = false;
-        }
+
+void checkJoystickXAxisMovement(const int pinX, const int minThreshold, const int maxThreshold, bool &joyMoved, int number[], int currentPos){
+    xValue = analogRead(pinX);
+    if( xValue < minThreshold && joyMoved == false){
+      if(number[currentPos] >0) {
+        number[currentPos]--;
       }
+      else{
+        number[currentPos] = 9;
+      }
+      joyMoved = true;
+    }
+    if(xValue > maxThreshold && joyMoved == false){
+      if(number[currentPos] <9) {
+        number[currentPos]++;
+      }
+      else{
+        number[currentPos] = 0;
+      }
+      joyMoved = true;
+    }
+    if(xValue >= minThreshold && xValue <= maxThreshold){
+      joyMoved = false;
     }
   }
+  
+void checkJoystickYAxisMovement(const int pinY, const int minThreshold,const  int maxThreshold, bool &joyMoved, int &currentPos){
+    yValue = analogRead(pinY);
+    if( yValue < minThreshold && joyMoved == false){
+      if(currentPos > 0) {
+        currentPos--;
+      }
+      else{
+        currentPos = 3;
+      }
+      joyMoved = true;
+    }
+    if(yValue > maxThreshold && joyMoved == false){
+      if(currentPos <3) {
+        currentPos++;
+      }
+      else{
+        currentPos = 0;
+      }
+      joyMoved = true;
+    }
+    if(yValue >= minThreshold && yValue <= maxThreshold){
+      joyMoved = false;
+    }
+}
 
-  lastSwState = reading;
+
+void buttonPressed(){
+  isLockedInOnDigit = !isLockedInOnDigit;
+  
+  for (int i = 0; i < numberLength; ++i )
+     EEPROM.write ( i, ledNumber[i]);
 }
 
 void writeNumber(int number[],int numberLength, int displayPos, bool isSelected){
-  bool dpSegments[4] = {false,false,false,false};
-  dpSegments[displayPos] = true;
-  for(int currentPos = 0; currentPos < numberLength; currentPos++){
+  bool dpOn = false;
+  for(int currentPos = 0; currentPos < numberLength; ++currentPos){
+      dpOn = false;
       showDigit(numberLength - 1 - currentPos);
       if(currentPos == displayPos){
         if(isSelected == true){
-          dpSegments[displayPos] = true;
+          dpOn = true;
         }
         else if( millis() - dpBlinkingTime > dpBlinkingInterval){
-          dpSegments[displayPos] = !dpSegments[displayPos];
+          dpOn = !dpOn;
           dpBlinkingTime = millis(); 
         }
       }
-      
-        
       int digit = digitArray[number[currentPos]];
-      if(dpSegments[currentPos] == true) {
+      if(dpOn == true) {
         digit++;
       }
       writeRegister(digit);
-      delay(1);
+      delay(5);
     }
 }
 
 void showDigit(int digit){
-  for(int i = 0; i < displayCount; i++){
+  for(int i = 0; i < displayCount; ++i){
     digitalWrite(displayDigits[i], HIGH);
   };
   digitalWrite(displayDigits[digit], LOW);
